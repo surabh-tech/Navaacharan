c8c5cff7772417f011897ed334533570b84e04f7
sujit <Sujit-magar>
2024-10-30 14:53:09 +0530

    changes 1

---
+++ footer.php
@@ -0,0 +1,80 @@
+<!-- start footer -->
+<footer class="footer-dark bg-slate-blue p-0">
+            <div class="d-none d-md-flex">  
+                <div class="overlap-section-one-fourth animation-float">
+                    <img src="images/demo-startup-bg-01.png" alt="">
+                </div> 
+            </div>
+            <div class="container">
+                <div class="row justify-content-center pt-4 lg-pt-2 sm-pt-45px">
+                    <!-- start footer column -->
+                    <div class="col-6 col-xl-3 col-lg-12 col-sm-6 last-paragraph-no-margin text-xl-start text-lg-center order-sm-1 lg-mb-50px sm-mb-35px">
+                        <a href="demo-startup.html" class="footer-logo mb-20px md-mb-10px d-inline-block" aria-label="logo">
+                            <img src="images/navaacharan-labs-logo-white.png" alt="Navaacharan Labs Logo">
+                        </a>
+                        <p class="w-90 lg-w-40 md-w-100 mx-lg-auto mx-xl-0">Navaacharan Labs specializes in cutting-edge research and development to drive innovation in technology.</p>
+                        <div class="elements-social social-icon-style-02 mt-20px">
+                            <ul class="small-icon light">
+                                <li class="my-0"><a class="facebook" href="https://www.facebook.com/" target="_blank" aria-label="facebook"><i class="fa-brands fa-facebook-f"></i></a></li>
+                                <li class="my-0"><a class="twitter" href="http://www.twitter.com" target="_blank" aria-label="twitter"><i class="fa-brands fa-twitter"></i></a></li> 
+                                <li class="my-0"><a class="instagram" href="http://www.instagram.com" target="_blank" aria-label="instagram"><i class="fa-brands fa-instagram"></i></a></li> 
+                            </ul>
+                        </div>
+                    </div>
+                    <!-- end footer column -->
+                    <!-- start footer column -->
+                    <div class="col-6 col-xl-2 col-lg-3 col-sm-4 xs-mb-35px order-sm-3 order-lg-2">
+                        <span class="alt-font fw-500 d-block text-white mb-10px">About Us</span>
+                        <ul>
+                            <li><a href="demo-startup-who-we-are.html">Who We Are</a></li>
+                            <li><a href="demo-startup-services.html">Our Services</a></li>
+                            <li><a href="demo-startup-clients.html">Our Clients</a></li>
+                            <li><a href="demo-startup-contact.html">Contact Us</a></li>
+                        </ul>
+                    </div>
+                    <!-- end footer column -->
+                    <!-- start footer column -->
+                    <div class="col-6 col-xl-2 col-lg-3 col-md-5 col-sm-4 xs-mb-35px order-sm-4 order-lg-3">
+                        <span class="alt-font fw-500 d-block text-white mb-10px">Services</span>
+                        <ul>
+                            <li><a href="demo-startup-services-details.html">Research & Development</a></li>
+                            <li><a href="demo-startup-services-details.html">Product Innovation</a></li>
+                            <li><a href="demo-startup-services-details.html">Technology Solutions</a></li>
+                        </ul>
+                    </div>
+                    <!-- end footer column -->
+                    <!-- start footer column -->
+                    <div class="col-6 col-xl-2 col-lg-3 col-md-3 col-sm-4 xs-mb-35px order-sm-5 order-lg-4">
+                        <span class="alt-font fw-500 d-block text-white mb-10px">Get in Touch</span>
+                        <span>Call Us Directly</span>
+                        <span class="d-block mb-15px"><a class="text-white" href="tel:1235678901">(123) 567 8901</a></span>
+                        <span>Email Support</span>
+                        <a href="mailto:support@navaacharanlabs.com" class="text-white text-decoration-line-bottom">support@navaacharanlabs.com</a>
+                    </div>
+                    <!-- end footer column -->                    
+                </div>
+                <div class="row justify-content-center align-items-center pt-5 sm-pt-35px">
+                    <!-- start divider -->
+                    <div class="col-12">
+                        <div class="divider-style-03 divider-style-03-01 border-color-transparent-white-light"></div>
+                    </div>
+                    <!-- end divider -->
+                    <!-- start copyright -->
+                    <div class="col-lg-5 pt-25px pb-25px md-pt-0 fs-16 order-2 order-lg-1 text-center text-lg-start last-paragraph-no-margin">
+                        <p>&copy; 2024 Navaacharan Labs.</p>
+                    </div>
+                    <!-- end copyright -->
+                    <!-- start footer menu -->
+                    <div class="col-lg-7 pt-25px pb-25px md-pb-5px fs-16 order-1 order-lg-2 text-center text-lg-end">
+                        <ul class="footer-navbar sm-lh-normal"> 
+                            <li><a href="#" class="nav-link">Privacy Policy</a></li>
+                            <li><a href="#" class="nav-link">Terms and Conditions</a></li>
+                            <li><a href="#" class="nav-link">Copyright</a></li>
+                        </ul>
+                    </div>
+                    <!-- end footer menu -->
+                </div>
+            </div> 
+        </footer>
+        
+        <!-- end footer -->
