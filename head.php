b0da5927db1821c536d689d38528595ec7fec954
unknown <saurabh2003nalawade@gmail.com>
2024-11-17 13:08:46 +0530

    Nav Bar done

--- head.php
+++ head.php
@@ -19,4 +19,7 @@
         <link rel="stylesheet" href="css/style.css"/>
         <link rel="stylesheet" href="css/responsive.css"/>
         <link rel="stylesheet" href="demos/startup/startup.css" />
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+
     </head>
--- header.php
+++ header.php
@@ -1,42 +1,51 @@
 <header> 
-    <!-- start navigation -->
-    <nav class="navbar navbar-expand-lg header-transparent bg-transparent border-bottom border-color-transparent-white-light disable-fixed">
-        <div class="container-fluid">
-            <div class="col-auto col-lg-2 me-lg-0 me-auto">
-                <a class="navbar-brand" href="index.php">
-                <img src="images/labs/headlogo1.png" data-at2x="images/labs/headlogo1.png" alt="" class="default-logo">
+            <!-- start navigation -->
+            <nav class="navbar navbar-expand-lg header-transparent bg-transparent border-bottom border-color-transparent-white-light disable-fixed">
+                <div class="container-fluid">
+                    <div class="col-auto col-lg-2 me-lg-0 me-auto">
+                        <a class="navbar-brand" href="demo-startup.html">
+                            <img src="images/labs/headlogo1.png" data-at2x="images/labs/headlogo1.png" alt="" class="default-logo">
                             <img src="images/labs/headlogo1.png" data-at2x="images/labs/headlogo1.png" alt="" class="alt-logo">
                             <img src="images/labs/headlogo1.png" data-at2x="images/labs/headlogo1.png" alt="" class="mobile-logo"> 
-
-               
-            </div>
-            <div class="col-auto menu-order position-static">
-                <button class="navbar-toggler float-start" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-line"></span>
-                    <span class="navbar-toggler-line"></span>
-                    <span class="navbar-toggler-line"></span>
-                    <span class="navbar-toggler-line"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarNav"> 
-                    <ul class="navbar-nav alt-font"> 
-                        <li class="nav-item"><a href="index.php" class="nav-link">Home</a></li>
-                        <li class="nav-item"><a href="who-we-are.php" class="nav-link">Who we are</a></li>
-                        <li class="nav-item dropdown dropdown-with-icon">
-                            <a href="services.php" class="nav-link">Services</a>
-                            <i class="fa-solid fa-angle-down dropdown-toggle" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false"></i>
-                        </li>
-                        <li class="nav-item"><a href="clients.php" class="nav-link">Clients</a></li>
-                        <li class="nav-item"><a href="contact.php" class="nav-link">Contact</a></li>
-                    </ul>
-                </div>
-            </div>
-            <div class="col-auto col-lg-2 text-end md-pe-0">
-                <div class="header-icon">
-                    <div class="header-push-button icon">
-                        
+                        </a>
                     </div>
-                </div>  
-            </div>
-        </div>
-    </nav>
-</header>
+                    <div class="col-auto menu-order position-static">
+                        <button class="navbar-toggler float-start" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-line"></span>
+                            <span class="navbar-toggler-line"></span>
+                            <span class="navbar-toggler-line"></span>
+                            <span class="navbar-toggler-line"></span>
+                        </button>
+                        <div class="collapse navbar-collapse" id="navbarNav"> 
+                            <ul class="navbar-nav alt-font"> 
+                                <li class="nav-item"><a href="index.php" class="nav-link">Home</a></li>
+                                <li class="nav-item"><a href="who-we-are.php" class="nav-link">Who we are</a></li>
+                             
+                                    <a href="services.php" class="nav-link">Services</a>
+                                
+                                <li class="nav-item"><a href="clients.php" class="nav-link">Clients</a></li>
+                               
+                                <li class="nav-item"><a href="contact.php" class="nav-link">Contact</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-auto col-lg-2 text-end md-pe-0">
+                        <div class="header-icon">
+                            
+                            <div class="header-push-button icon">
+                                <div class="push-button">
+                                    <span></span>
+                                    <span></span>
+                                    <span></span>
+                                    <span></span> 
+                                </div>
+                            </div>
+                        </div>  
+                    </div>
+                </div>
+            </nav>
+            <!-- end navigation -->
+            <!-- start push popup -->
+            
+            <!-- end push popup -->
+        </header>
